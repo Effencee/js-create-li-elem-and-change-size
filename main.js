fb44e4c34cfec44aa74da6a60d2a7a81bdc356c4
Effencee <107486971+Effencee@users.noreply.github.com>
2022-06-14 17:47:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
-// tutaj rozwiązanie
 const btn = document.querySelector('button');
 let number = 10;
 const changeLiFontSizeAndDisplay = () => {
     const li = [...document.querySelectorAll('li')];
-    // for (let i = 0; i < li.length; i++) {
-    //     li[i].style.display = 'block';
-    //     li[i].style.fontSize = number + 'px';
-    // }
     li.forEach(item => {
         item.style.display = 'block';
         item.style.fontSize = number + 'px';
